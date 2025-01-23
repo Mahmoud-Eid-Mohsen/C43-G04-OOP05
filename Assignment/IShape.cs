15f4f9161176c20f6eaa905ca01eb7330fae7c42
admin <admin@DESKTOP-JKD834T>
2025-01-23 11:26:11 +0200

    Assignment

---
+++ Assignment/IShape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal interface IShape
+    {
+        #region property
+        double Area { get; }
+        #endregion
+        #region method
+        void DisplayShapeInfo();
+        #endregion
+        
+    }
+}
---
+++ Assignment/Q3/PushNotificationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Q3
+{
+    
+    
+        public class PushNotificationService : INotificationService
+        {
+            public void SendNotification(string recipient, string message)
+            {
+                Console.WriteLine($"Sending Push Notification to {recipient}: {message}");
+            }
+        }
+   
+}
